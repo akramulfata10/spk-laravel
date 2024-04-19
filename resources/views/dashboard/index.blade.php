97e1d499895185b754e19f076bb5794099169087
akramulfata10 <superlay582@gmail.com>
2024-04-19 17:19:16 +0700

    teemplate admin

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('dashboard.layout.main')
+
+@section('container')
+    <h1> welcome to SPK menggunakan metode Saw</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('index');
+});
+
+Auth::routes();
